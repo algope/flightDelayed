e3c77c897890748e3fb22973e5f943ae276b6a9c
Alejandro González <alejandro.gon.per@gmail.com>
2016-12-05 14:49:26 +0100

    Added sparksql support

--- src/main/scala/org/upm/spark/flightdelayed/App.scala
+++ src/main/scala/org/upm/spark/flightdelayed/App.scala
@@ -2,16 +2,25 @@ package org.upm.spark.flightdelayed
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
 import org.apache.spark._
 import org.apache.log4j.{Level, Logger}
 
 object App {
   def main(args: Array[String]) {
     Logger.getRootLogger().setLevel(Level.WARN)
+    val inputPath = args{0}
     val conf = new SparkConf().setAppName("My first Spark application")
     val sc = new SparkContext(conf)
-    val data = sc.textFile("hdfs:///tmp/data/2008.csv")
-    //println(data.count())
-    data.take(20).foreach(println)
+
+    //val data = sc.textFile("hdfs:///tmp/data/2008.csv")
+    //val data = sc.textFile(inputPath)
+
+    val sparkSQL = SparkSession.builder().enableHiveSupport().getOrCreate()
+
+    val inputData = sparkSQL.read.csv(inputPath)
+    inputData.printSchema()
+    inputData.show()
+
   }
 }
